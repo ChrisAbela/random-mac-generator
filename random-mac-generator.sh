7c7277a26998ae38722c669dd74862cb99d64cf2
ChrisAbela <ChrisAbela@users.noreply.github.com>
2016-01-25 08:55:25 +0100

    Update random-mac-generator.sh

--- random-mac-generator.sh
+++ random-mac-generator.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2014, 2015, Chris Abela, Malta
+# Copyright (c) 2014, 2015, Chris Abela <kristofru@gmail.com>, Malta
 # All rights reserved.
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
